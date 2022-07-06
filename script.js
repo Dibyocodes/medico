7b526e1777da0708410e64c4d6fc6af77fcd992d
Dibyocodes <94511176+Dibyocodes@users.noreply.github.com>
2022-07-06 23:28:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,32 @@
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
-let header = document.querySelector('.header-2');
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoid2ViZGV2c2ltcGxpZmllZCIsImEiOiJja2d1c2x2djAwODE1MnltaGNzeHljcWN4In0.4u6YymF-wOIYpDoUTMcNOQ"
 
-menu.addEventListener('click', () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-});
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true
+})
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-
-    if(window.scrollY > 150){
-        header.classList.add('active');
-    }else{
-        header.classList.remove('active');
-    }
-
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude])
 }
 
-let countDate = new Date('june 1, 2021 00:00:00').getTime();
-
-function CountDown(){
-
-    let now = new Date().getTime();
-    gap = countDate - now;
-
-    let second = 1000;
-    let minute = second * 60;
-    let hour = minute * 60;
-    let day = hour * 24;
-
-    let d = Math.floor(gap / (day));
-    let h = Math.floor((gap % (day)) / (hour));
-    let m = Math.floor((gap % (hour)) / (minute));
-    let s = Math.floor((gap % (minute)) / (second));
-
-    document.getElementById('day').innerText = d;
-    document.getElementById('hour').innerText = h;
-    document.getElementById('minute').innerText = m;
-    document.getElementById('second').innerText = s;
-
+function errorLocation() {
+  setupMap([-2.24, 53.48])
 }
 
-setInterval(function(){
-    CountDown();
-},1000)
+function setupMap(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 15
+  })
+
+  const nav = new mapboxgl.NavigationControl()
+  map.addControl(nav)
+
+  var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+  })
+
+  map.addControl(directions, "top-left")
+}
